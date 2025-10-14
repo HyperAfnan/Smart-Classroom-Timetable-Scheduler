6ce7ae254bdadf286b6e88f47164d2d061a4814f
HyperAfnan <afnanmalik2712@gmail.com>
2025-10-14 10:30:39 +0530

    Added dist dri

---
+++ frontend/dist/assets/alert-CjM6HumU.js
@@ -0,0 +1,11 @@
+import{c as e}from"./createLucideIcon-De5o52GV.js";import{j as r}from"./index-DMwWV6LZ.js";import{a as s,d as i}from"./button-DdLPv1v6.js";/**
+ * @license lucide-react v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],p=e("check",o);/**
+ * @license lucide-react v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],v=e("loader-circle",d),l=i("relative w-full rounded-lg border px-4 py-3 text-sm grid has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] grid-cols-[0_1fr] has-[>svg]:gap-x-3 gap-y-0.5 items-start [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current",{variants:{variant:{default:"bg-card text-card-foreground",destructive:"text-destructive bg-card [&>svg]:text-current *:data-[slot=alert-description]:text-destructive/90"}},defaultVariants:{variant:"default"}});function x({className:t,variant:a,...c}){return r.jsx("div",{"data-slot":"alert",role:"alert",className:s(l({variant:a}),t),...c})}function f({className:t,...a}){return r.jsx("div",{"data-slot":"alert-description",className:s("text-muted-foreground col-start-2 grid justify-items-start gap-1 text-sm [&_p]:leading-relaxed",t),...a})}export{x as A,p as C,v as L,f as a};
