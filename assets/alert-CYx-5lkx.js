b770e86de6e5506d9415029ef478e15f04dabc3a
HyperAfnan <afnanmalik2712@gmail.com>
2025-11-01 00:58:29 +0530

    Updates

---
+++ assets/alert-CYx-5lkx.js
@@ -0,0 +1,6 @@
+import{c}from"./createLucideIcon-BsWjkkVR.js";import{r as e}from"./index-DgwETXXl.js";import{c as i}from"./index-Ciz9C784.js";import{a}from"./utils-CHUdG8Hq.js";/**
+ * @license lucide-react v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],m=c("loader-circle",o),d=i("relative w-full rounded-lg border px-4 py-3 text-sm grid has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] grid-cols-[0_1fr] has-[>svg]:gap-x-3 gap-y-0.5 items-start [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current",{variants:{variant:{default:"bg-card text-card-foreground",destructive:"text-destructive bg-card [&>svg]:text-current *:data-[slot=alert-description]:text-destructive/90"}},defaultVariants:{variant:"default"}});function p({className:t,variant:r,...s}){return e.createElement("div",{"data-slot":"alert",role:"alert",className:a(d({variant:r}),t),...s})}function v({className:t,...r}){return e.createElement("div",{"data-slot":"alert-description",className:a("text-muted-foreground col-start-2 grid justify-items-start gap-1 text-sm [&_p]:leading-relaxed",t),...r})}export{p as A,m as L,v as a};
